219eea14ee9ef2c025791e1f32d74c078a055e35
Kevin4091 <kevinchacko4091@gmail.com>
2024-06-26 22:59:13 +0530

    updated

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:task/view/home_screen/home_screen.dart';
 import 'package:task/view/register_screen/register_screen.dart';
 
@@ -7,8 +8,11 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<FormState> _emailkey = GlobalKey<FormState>();
+    GlobalKey<FormState> _passkey1 = GlobalKey<FormState>();
+
     TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
+    TextEditingController passcontroller1 = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -17,99 +21,146 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
         child: Column(
           children: [
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
-                prefixIcon: const Icon(Icons.person),
-                contentPadding: const EdgeInsets.all(8),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            const SizedBox(
+              height: 40,
+            ),
+            const Center(
+              child: Text(
+                'Sign  in to Your Account',
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+              ),
+            ),
+            Form(
+              key: _emailkey,
+              child: TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: const Icon(Icons.person),
+                  contentPadding: const EdgeInsets.all(8),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
               ),
             ),
             const SizedBox(
               height: 20,
             ),
-            TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.remove_red_eye_rounded),
-                  labelText: "Password",
-                  contentPadding: const EdgeInsets.all(10),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40))),
+            Form(
+              key: _passkey1,
+              child: TextFormField(
+                controller: passcontroller1,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.remove_red_eye_rounded),
+                    labelText: "Password",
+                    contentPadding: const EdgeInsets.all(10),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40))),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              children: [
+                Icon(
+                  Icons.check_box_outline_blank_outlined,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                Text(
+                  'Remember Me',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17,
+                  ),
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 17,
+                  ),
+                ),
+              ],
             ),
             const SizedBox(
               height: 40,
             ),
-            loginProvider.isloading == true
-                ? const CircularProgressIndicator()
-                : InkWell(
-                    onTap: () async {
-                      if (emailController.text.isNotEmpty &&
-                          passwordController.text.isNotEmpty) {
-                        context
-                            .read<LoginScreenController>()
-                            .onLogin(
-                                context: context,
-                                email: emailController.text,
-                                password: passwordController.text)
-                            .then((value) {
-                          if (value == true) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    backgroundColor: Colors.green,
-                                    content: Text("Login Sucess")));
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const HomeScreen(),
-                                ),
-                                (route) => false);
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    backgroundColor: Colors.red,
-                                    content: Text("Login Failed")));
-                          }
-                        });
-                      }
-                    },
-                    child: Container(
-                      height: 50,
-                      width: 150,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blue),
-                      child: const Center(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 20),
-                        ),
+            InkWell(
+              onTap: () {
+                if (_emailkey.currentState!.validate() &&
+                    _passkey1.currentState!.validate()) {
+                  if (emailController.text == storedEmail &&
+                      passcontroller1.text == storedPassword) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomeScreen(),
+                        ));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      backgroundColor: Colors.red,
+                      content: const Text(
+                        'Invalid Credentials',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
                       ),
-                    ),
+                    ));
+                  }
+                }
+              },
+              child: Container(
+                height: 60,
+                width: 400,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 19, 124, 209),
+                  borderRadius: BorderRadius.circular(13),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15),
                   ),
+                ),
+              ),
+            ),
             const SizedBox(
-              height: 70,
+              height: 300,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text(
-                  "You Dont Have Account?",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  "Don't have an account?",
+                  style: TextStyle(color: Colors.black, fontSize: 15),
                 ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
-                          ));
-                    },
-                    child: const Text("RegisterNow"))
+                InkWell(
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterScreen(),
+                      ),
+                      (route) => false,
+                    );
+                  },
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.blue, fontSize: 15),
+                  ),
+                ),
               ],
             )
           ],
--- lib/view/register_screen/register_screen.dart
+++ lib/view/register_screen/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:task/view/login_screen/login_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -6,8 +7,13 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
+    GlobalKey<FormState> _emailkey = GlobalKey<FormState>();
+    GlobalKey<FormState> _passkey1 = GlobalKey<FormState>();
+    GlobalKey<FormState> _passkey2 = GlobalKey<FormState>();
+    TextEditingController regemailcontroller = TextEditingController();
+    TextEditingController regpasscontroller1 = TextEditingController();
+    TextEditingController passcontroller2 = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -15,106 +21,150 @@ class RegisterScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
-                prefixIcon: const Icon(Icons.person),
-                contentPadding: const EdgeInsets.all(8),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: Text(
+                'Sign Up for Free',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Form(
+              key: _emailkey,
+              child: TextFormField(
+                controller: regemailcontroller,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: const Icon(Icons.person),
+                  contentPadding: const EdgeInsets.all(8),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
               ),
             ),
             const SizedBox(
               height: 20,
             ),
-            TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.remove_red_eye_rounded),
-                  labelText: "Password",
-                  contentPadding: const EdgeInsets.all(10),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40))),
+            Form(
+              key: _passkey1,
+              child: TextFormField(
+                controller: regpasscontroller1,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.remove_red_eye_rounded),
+                    labelText: " Your Password",
+                    contentPadding: const EdgeInsets.all(10),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40))),
+              ),
             ),
-            const SizedBox(
+            Form(
+              key: _passkey2,
+              child: TextFormField(
+                controller: passcontroller2,
+                validator: (value) {
+                  if (regpasscontroller1.text == passcontroller2.text) {
+                    return null;
+                  } else {
+                    return 'Password does not Match';
+                  }
+                },
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                ),
+                obscureText: true,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                        borderSide: BorderSide(
+                          color: Colors.grey.withOpacity(.8),
+                          width: 2,
+                        )),
+                    suffixIcon: Icon(
+                      Icons.visibility_off,
+                      size: 20,
+                      color: Colors.grey.withOpacity(.8),
+                    ),
+                    hintText: 'Your Confirm Password',
+                    hintStyle: TextStyle(
+                      color: Colors.grey.withOpacity(.8),
+                      fontSize: 20,
+                    ),
+                    errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                        borderSide: BorderSide(
+                          color: Colors.red,
+                          width: 2,
+                        ))),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            SizedBox(
               height: 40,
             ),
-            providerobj.isloading
-                ? const CircularProgressIndicator()
-                : InkWell(
-                    onTap: () async {
-                      if (emailController.text.isNotEmpty &&
-                          passwordController.text.isNotEmpty) {
-                        context
-                            .read<RegistrationScreenController>()
-                            .register(
-                                context: context,
-                                email: emailController.text,
-                                password: passwordController.text)
-                            .then((value) {
-                          if (value == true) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    backgroundColor: Colors.green,
-                                    content: Text("Registraion Sucess")));
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const LoginScreen(),
-                                ),
-                                (route) => false);
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    backgroundColor: Colors.red,
-                                    content: Text("Registration failed")));
-                          }
-                        });
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                backgroundColor: Colors.red,
-                                content:
-                                    Text("Enter a Valid email and password")));
-                      }
-                    },
-                    child: Container(
-                      height: 50,
-                      width: 150,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.blue),
-                      child: const Center(
-                        child: Text(
-                          "Register",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 20),
-                        ),
-                      ),
-                    ),
+            InkWell(
+              onTap: () {
+                if (_emailkey.currentState!.validate() &&
+                    _passkey1.currentState!.validate() &&
+                    _passkey2.currentState!.validate()) {
+                  storedEmail = regemailcontroller.text;
+                  storedPassword = regpasscontroller1.text;
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen(),
+                      ));
+                }
+              },
+              child: Container(
+                height: 60,
+                width: 400,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 19, 124, 209),
+                  borderRadius: BorderRadius.circular(13),
+                ),
+                child: Center(
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15),
                   ),
-            const SizedBox(
-              height: 70,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 250,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  "You  Have Account?",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                Text(
+                  "Already have an account?",
+                  style: TextStyle(color: Colors.black, fontSize: 15),
                 ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginScreen(),
-                          ));
-                    },
-                    child: const Text("Login now"))
+                InkWell(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginScreen(),
+                        ));
+                  },
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.blue, fontSize: 15),
+                  ),
+                ),
               ],
             )
           ],
@@ -122,4 +172,9 @@ class RegisterScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class reg {
+  static String? EmailController;
+  static String? Pass1Controller;
 }
